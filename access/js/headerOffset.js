2a512e34ac26078b892b8236dab1e7cd7917d9db
Melkor <gadhiev-2019@bk.ru>
2023-11-09 21:25:33 +0300

    c2 for 9prak rckchir

--- access/js/headerOffset.js
+++ access/js/headerOffset.js
@@ -32,6 +32,74 @@ function randomDelay(index) {
 
 
 
+// 9 практика
+
+//1
+const answer = prompt("Желаете пройти регистрацию на сайте?");
+if (answer === "Да") {
+    alert("Круто!");
+} else {
+    alert("Попробуй ещё раз");
+}
+
+//2
+const login = prompt("Введите логин:");
+if (login === "Админ") {
+    const password = prompt("Введите пароль:");
+    if (password === "Я главный") {
+        alert("Здравствуйте!");
+    } else if (password === null) {
+        alert("Отменено");
+    } else {
+        alert("Неверный пароль");
+    }
+} else if (login === null) {
+    alert("Отменено");
+} else {
+    alert("Я вас не знаю");
+}
+
+//3
+let isLiked = false;
+
+function changeColor() {
+    const button = document.getElementById("likeButton");
+    if (isLiked) {
+        button.style.backgroundColor = "";
+        isLiked = false;
+    } else {
+        button.style.backgroundColor = "green";
+        isLiked = true;
+    }
+}
+
+//4
+let isDrawing = false;
+function toggleDrawing() {
+    isDrawing = !isDrawing;
+    const drawButton = document.getElementById("drawButton");
+    drawButton.textContent = isDrawing ? "Остановить рисование" : "Начать рисование";
+
+    if (isDrawing) {
+        document.addEventListener("mousemove", drawElement);
+    } else {
+        document.removeEventListener("mousemove", drawElement);
+    }
+}
+
+function drawElement(event) {
+    console.log(event);
+    const element = document.createElement("div");
+    element.style.position = "absolute";
+    element.style.borderRadius = "50%";
+    element.style.left = event.clientX + "px";
+    element.style.top = event.clientY + "px";
+    element.style.width = "10px";
+    element.style.height = "10px";
+    element.style.backgroundColor = "#5f9b60";
+    document.querySelector("body").appendChild(element);
+}
+
 // function delay(ms) {
 //     return new Promise(function (resolve, reject) {
 //         setTimeout(() => {
